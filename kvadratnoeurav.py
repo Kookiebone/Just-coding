4d566381a35a2c42620f6e88a76961a8f1aef738
Kookiebone <114737993+Kookiebone@users.noreply.github.com>
2022-09-30 15:30:38 +0500

    Update kvadratnoeurav.py

--- kvadratnoeurav.py
+++ kvadratnoeurav.py
@@ -1,4 +1,4 @@
-print("Введите три аргумента по порядку a, b и c:")
+print("Please, input there 3 arguments - a, b and c:")
 a = float(input())
 b = float(input())
 c = float(input())
@@ -6,30 +6,30 @@ a1 = str(a)
 b1 = str(b)
 c1 = str(c)
 if (a and b and c) != 0:
-  print("Ваше уравнение аыглядит так:")
+  print("Your equations seems:")
   print(a1+"*x² + "+b1+"*x + "+c1+" = 0")
   D = (b**2 - 4*a*c)
   x1 = (-b + D**0.5) / (a*2)
   x2 = (-b - D**0.5) / (a*2)
-  print("Корни классического квадратного уравнения:")
+  print("Roots of this equation:")
   print(x1, x2)
 if a == 0:
-  print("Ваше уравнение выглядит так:")
+  print("Your quadratic equation:")
   print(b1+"*x + "+c1+" = 0")
   x = -c /b1
-  print("Корень линейного уравнения:")
+  print("Root of this equation:")
   print(x)
 if b == 0:
-  print("Ваше уравнение выглядит так:")
+  print("Your equation seems as following:")
   print(a1+"*x² + "+c1+" = 0")
   x1 = (-c)**0.5
   x2 = -(-c)**0.5
-  print("Корни уравнения выглядят так:")
+  print("Roots of this equation:")
   print(x1, x2)
 if c == 0:
-  print("Ваше уравнение выглядит так:")
+  print("Your equestion:")
   print(a1+"*x² + "+b1+"*x = 0")
   x1 = 0
   x2 = -b / a
-  print("Корни вашего уравнения выглядят так:")
+  print("Roots of this equation:")
   print(x1, x2)
